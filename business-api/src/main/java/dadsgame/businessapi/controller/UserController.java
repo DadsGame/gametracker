b6008e873d6e6cd1f1bf3d285330b0e927b5378f
Galimede <mat.degand@outlook.fr>
2022-05-30 18:15:15 +0200

    :goal_net: throw error if user already exists

--- business-api/src/main/java/dadsgame/businessapi/controller/UserController.java
+++ business-api/src/main/java/dadsgame/businessapi/controller/UserController.java
@@ -1,6 +1,7 @@
 package dadsgame.businessapi.controller;
 
 import dadsgame.businessapi.entity.UserEntity;
+import dadsgame.businessapi.exception.AlreadyExistsException;
 import dadsgame.businessapi.service.userService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,7 +27,10 @@ public class UserController {
     public List<UserEntity> getAllUsers() { return userService.getAllUser(); }
 
     @PostMapping("/sign-up")
-    public ResponseEntity signUp(@RequestBody UserEntity userEntity) {
+    public ResponseEntity signUp(@RequestBody UserEntity userEntity)  {
+        if(userService.findByUserName(userEntity.getUsername()) != null) {
+            throw new AlreadyExistsException("Account already exists.");
+        }
         userEntity.setPassword(bCryptPasswordEncoder.encode(userEntity.getPassword()));
         userService.createUser(userEntity);
         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
@@ -32,11 +38,6 @@ public class UserController {
 
     @GetMapping("/{idUser}")
     public Optional<UserEntity> getUserById(@PathVariable int idUser) { return userService.getUserById(idUser); }
-
-    @PostMapping
-    public UserEntity createUser(@RequestBody UserEntity newUserEntity){
-        return userService.createUser(newUserEntity);
-    }
 
     @PutMapping("/{idUser}")
     public ResponseEntity<UserEntity> updateUser(@RequestBody UserEntity userEntity){
---
+++ business-api/src/main/java/dadsgame/businessapi/exception/AlreadyExistsException.java
@@ -0,0 +1,11 @@
+package dadsgame.businessapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AlreadyExistsException extends RuntimeException {
+    public AlreadyExistsException(String s) {
+        super(s);
+    }
+}
